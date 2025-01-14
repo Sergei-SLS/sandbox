0e126ada186a9389485770605b4d26fb1b781da5
Sergei <lyamin.sls@gmail.com>
2025-01-14 16:54:57 +0500

    done

--- codewars/oppositesAttract.js
+++ codewars/oppositesAttract.js
@@ -8,8 +8,8 @@
 
 function lovefunc(flower1, flower2){
     if ((flower1 % 2) == 1 && (flower2 % 2) == 0 || (flower2 % 2) == 1 && (flower1 % 2) == 0) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   };
