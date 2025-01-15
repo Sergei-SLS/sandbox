6980bbc48ee2b7eadf07549d666bb7ce89100363
Sergei <lyamin.sls@gmail.com>
2025-01-15 22:26:34 +0500

    task completed

---
+++ freeCodeCamp/tasks/js/step31.js
@@ -0,0 +1,13 @@
+// Step 31
+// Declaring a variable with the let keyword allows it to be reassigned. This means you could change character later to be a completely different value.
+
+// For this project, you will not want to change these variable values. So instead, you should use const to declare them. const variables are special.
+
+// First, a const variable cannot be reassigned like a let variable.
+
+// Replace your let keywords with const.
+
+const character = "Hello";
+const count = 8;
+const rows = [];
+
